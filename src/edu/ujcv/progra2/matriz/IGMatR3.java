2f2ac33c61114fb7f1cd65caf1a8094bec1afdcf
rich23rl <52242920+rich23rl@users.noreply.github.com>
2019-07-14 22:40:23 -0600

    Trabajo en MATRIZ-R3

--- src/edu/ujcv/progra2/matriz/IGMatR3.java
+++ src/edu/ujcv/progra2/matriz/IGMatR3.java
@@ -1,18 +1,32 @@
 package edu.ujcv.progra2.matriz;
 
+import edu.ujcv.progra2.vector.IGVecR3;
+
 public class IGMatR3 {
 
+    private IGVecR3 col1;
+    private IGVecR3 col2;
+    private IGVecR3 col3;
 
-    // public static IGVecR2 rotacionZ (double angulo, IGVecR2 vector){
-//
-//    }
+    public IGMatR3(){
+        col1 = new IGVecR3(1,0,0);
+        col2 = new IGVecR3(0,1,0);
+        col3 = new IGVecR3(0,0,1);
+    }
 
-    // public static IGVecR2 rotacionX (double angulo, IGVecR2 vector){
-//
-//    }
+    public IGMatR3(IGVecR3 col1, IGVecR3 col2, IGVecR3 col3){
+        this.col1 = new IGVecR3(col1);
+        this.col2 = new IGVecR3(col2);
+        this.col3 = new IGVecR3(col3);
+    }
 
-    // public static IGVecR2 rotacionY (double angulo, IGVecR2 vector){
-//
-//    }
+
+    public IGMatR3(IGMatR3 b){
+        this.col1 = new IGVecR3(b.col1);
+        this.col2 = new IGVecR3(b.col2);
+        this.col3 = new IGVecR3(b.col3);
+    }
+
+    
 
 }
