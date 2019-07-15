3c0c696a25e81f3828b1bc27ca5ac75a99b02f26
rich23rl <52242920+rich23rl@users.noreply.github.com>
2019-07-14 22:00:41 -0600

    Trabajo en MATRIZ-R2

--- src/edu/ujcv/progra2/matriz/IGMatR2.java
+++ src/edu/ujcv/progra2/matriz/IGMatR2.java
@@ -3,43 +3,45 @@ package edu.ujcv.progra2.matriz;
 import edu.ujcv.progra2.vector.IGVecR2;
 
 public class IGMatR2 {
-    // TODO: implementar.
+/*
+    double x1,y1;
+    double x2,y2;
 
-    //double x1,y1;
-    //double x2,y2;
+    double[][] m;
 
+    IGVecR2 f1;
+    IGVecR2 f2;
 
-    // double[][] m;
+ */
+    private IGVecR2 col1;
+    private IGVecR2 col2;
 
-    //IGVecR2 f1;
-    //IGVecR2 f2;
+    public IGMatR2(){
+        col1 = new IGVecR2(1,0);
+        col2 = new IGVecR2(0,1);
+    }
 
-    //IGVecR2 col1;
-    //IGVecR2 col2;
+    public IGMatR2(IGVecR2 col1, IGVecR2 col2){
+        this.col1 = new IGVecR2(col1);
+        this.col2 = new IGVecR2(col2);
+    }
 
+    public IGMatR2(IGMatR2 b){
+        this.col1 = new IGVecR2(b.col1);
+        this.col2 = new IGVecR2(b.col2);
+    }
+    /*
+    public IGMatR2 suma (IGMatR2 b){
 
+    }
 
-//    public IGMatR2 mul(IGMatR2 b){
-//
-//    }
-//
+    public IGMatR2 escalarXmat(double escalar){
 
+    }
 
-//    public IGVecR2 colXMat(IGVecR2 col){
-//
-//    }
+    public static IGVecR2 rotacion (double angulo, IGVecR2 vector){
 
-//    public IGMatR2 suma (IGMatR2 b){
-//
-//    }
-
-//    public IGMatR2 escalarXmat(double escalar){
-//
-//    }
-
-//    public static IGVecR2 rotacion (double angulo, IGVecR2 vector){
-//
-//    }
-
+    }
+*/
 
 }
