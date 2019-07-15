c7616c353f6e0e60a3c1fccfd2832b37963d9398
rich23rl <52242920+rich23rl@users.noreply.github.com>
2019-07-14 23:44:45 -0600

    Listo el LectorDeTecladoValido

--- src/edu/ujcv/progra2/utility/LectorDeTecladoValidado.java
+++ src/edu/ujcv/progra2/utility/LectorDeTecladoValidado.java
@@ -1,34 +1,31 @@
 package edu.ujcv.progra2.utility;
-
-//singleton design pattetrn.
-
-
-//        LectorDeTecladoValidado LDTV = LectorDeTecladoValidado.getInstance();
-//        double x = LDTV.getDouble("ingrese un numero real","intente de nuevo");
-//        System.out.println("su numero es: "+ x);
 /*
+singleton design pattetrn.
+        LectorDeTecladoValidado LDTV = LectorDeTecladoValidado.getInstance();
+        double x = LDTV.getDouble("ingrese un numero real","intente de nuevo");
+        System.out.println("su numero es: "+ x);
+*/
 import java.util.Scanner;
-
 public class LectorDeTecladoValidado {
 
     private static LectorDeTecladoValidado instance;
     private Scanner sc;
 
-    public static LectorDeTecladoValidado getInstance(){
-        if(instance == null){
+    public static LectorDeTecladoValidado getInstance() {
+        if (instance == null) {
             instance = new LectorDeTecladoValidado();
         }
         return instance;
     }
 
-    private LectorDeTecladoValidado(){
+    private LectorDeTecladoValidado() {
         sc = new Scanner(System.in);
     }
 
-    public double getDouble(String mensaje, String reintento){
+    public double getDouble(String mensaje, String reintento) {
 
         System.out.println(mensaje);
-        while(!sc.hasNextDouble()){
+        while (!sc.hasNextDouble()) {
             System.out.println(reintento);
             sc.nextLine();
         }
@@ -36,9 +33,9 @@ public class LectorDeTecladoValidado {
 
     }
 
-    public double getDouble(String reintento){
+    public double getDouble(String reintento) {
 
-        while(!sc.hasNextDouble()){
+        while (!sc.hasNextDouble()) {
             System.out.println(reintento);
             sc.nextLine();
         }
@@ -46,10 +43,10 @@ public class LectorDeTecladoValidado {
 
     }
 
-    public int getInteger(String mensaje, String reintento){
+    public int getInteger(String mensaje, String reintento) {
 
         System.out.println(mensaje);
-        while(!sc.hasNextDouble()){
+        while (!sc.hasNextDouble()) {
             System.out.println(reintento);
             sc.nextLine();
         }
@@ -57,9 +54,9 @@ public class LectorDeTecladoValidado {
 
     }
 
-    public int getInteger( String reintento){
+    public int getInteger(String reintento) {
 
-        while(!sc.hasNextDouble()){
+        while (!sc.hasNextDouble()) {
             System.out.println(reintento);
             sc.nextLine();
         }
@@ -67,5 +64,4 @@ public class LectorDeTecladoValidado {
 
     }
 
- */
-
+}
